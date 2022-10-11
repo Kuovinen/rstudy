59b29b1a15a4bbdc01ce679fa422a65912a017ca
RomanZujev <roman.zujev@gmail.com>
2022-10-11 20:59:15 +0300

    redemantary menu with working buttons added

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,25 @@ import React from "react";
 import "./App.css";
 import Postgress from "./components/Postgress";
 function App() {
+  const [theme, setTheme] = React.useState("sql");
+  const arrayOfTopics = [{ name: "sql", content: <Postgress /> }];
+
+  const content = arrayOfTopics.reduce((prev, current) => {
+    if (current.name == theme) {
+      return [...prev, current.content];
+    }
+    return prev;
+  }, [] as JSX.Element[]);
   return (
     <div className="App">
-      <Postgress />
+      <menu id="menu">
+        {" "}
+        <span>MENU:</span>{" "}
+        <div onClick={() => setTheme("sql")}>PostgresSQL</div>
+        <div onClick={() => setTheme("mongo")}>mongoDb</div>
+        <div>JS Functions</div>
+      </menu>{" "}
+      {content}
     </div>
   );
 }
