c9114e2aff7d2933ca803113b1a1c7f859df04a9
RomanZujev <roman.zujev@gmail.com>
2022-10-14 19:22:09 +0300

    added useRef

--- src/components/ReactComps/data.tsx
+++ src/components/ReactComps/data.tsx
@@ -7,9 +7,15 @@ let data = [
   },
   {
     name: `.useEffect()`,
-    form: `useEffect(()=>//main function,()=>//cleanerfunction,[dependancy]);`,
+    form: `React.useEffect(()=>//main function,()=>//cleanerfunction,[dependancy]);`,
     comment: `//run main function each time the sate of a dependancy changes and run cleaner function after`,
     returns: "RETURN: EXAMINE THIS CLOSELY!!",
+  },
+  {
+    name: `.useRef()`,
+    form: `const a=React.useRef(null); + <div ref={a}>InnerHTML<div>`,
+    comment: `//a way to save a value to state that does not later cause rerenders upon update`,
+    returns: "RETURN: object containing the value inside the '.current' key",
   },
 ];
 
