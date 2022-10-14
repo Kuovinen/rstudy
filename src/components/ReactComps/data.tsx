8c59c04b6d3fd1d7c8d24ff65ca3c286ba8441c0
RomanZujev <roman.zujev@gmail.com>
2022-10-14 19:31:29 +0300

    aded useContext and createContext

--- src/components/ReactComps/data.tsx
+++ src/components/ReactComps/data.tsx
@@ -17,6 +17,17 @@ let data = [
     comment: `//a way to save a value to state that does not later cause rerenders upon update`,
     returns: "RETURN: object containing the value inside the '.current' key",
   },
+  {
+    name: `.useContext()/.createContext`,
+    form: `const MyContext=React.createContext('value');//outside the function + 
+    return (
+        <MyContext.Provider value={'value2'}>
+          <Toolbar />
+        </MyContext.Provider>
+      );//inside function const recievedContext = useContext(MyContext);//inside child `,
+    comment: `//a way to pass props directly without prop drilling`,
+    returns: "-",
+  },
 ];
 
 export default data;
